c9124e53f4771004983c5c1889c82c43114bb667
Toni <antonia.c.white1@gmail.com>
2022-10-14 20:00:24 +0000

    Instate plants app and create plant models

---
+++ plants/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class PlantType(models.Model):
+    """Plant types"""
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class PlantDifficulty(models.Model):
+    """Plant care difficulties"""
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Plant(models.Model):
+    """Individual plant details"""
+
+    plant_type = models.ForeignKey(
+        'PlantType', null=True, blank=True, on_delete=models.SET_NULL)
+    plant_difficulty = models.ForeignKey(
+        'PlantDifficulty', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ plants/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+# Create your views here.
+
+
+def all_plants(request):
+    """ A view to show all products, including sorting and search queries """
+
+    plants = Plant.objects.all()
+
+    context = {
+        'plants': plants
+    }
+
+    return render(request, 'plants/plants.html', context)
