87669adc0569fc1b6ec2992ddbbade8126e98372
Toni <antonia.c.white1@gmail.com>
2022-10-15 15:36:40 +0000

    Register plant models in admin.py

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plant, PlantDifficulty, PlantType
 
 # Register your models here.
+admin.site.register(Plant)
+admin.site.register(PlantType)
+admin.site.register(PlantDifficulty)
