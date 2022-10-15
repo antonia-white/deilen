2abd3de3bd9025256089876904a07fc6b0fbc989
Toni <antonia.c.white1@gmail.com>
2022-10-15 15:53:49 +0000

    Customize plants admin in admin.py

--- plants/admin.py
+++ plants/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 from .models import Plant, PlantDifficulty, PlantType
 
 # Register your models here.
-admin.site.register(Plant)
-admin.site.register(PlantType)
-admin.site.register(PlantDifficulty)
+
+
+class PlantAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'plant_type',
+        'plant_difficulty',
+        'price',
+        'image',
+    )
+
+    ordering = ('plant_type', 'plant_difficulty')
+
+
+class PlantDifficultyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class PlantTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Plant, PlantAdmin)
+admin.site.register(PlantType, PlantTypeAdmin)
+admin.site.register(PlantDifficulty, PlantDifficultyAdmin)
