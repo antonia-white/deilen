20bfce8d782e0a999624073e70e4329601a2713d
Toni <antonia.c.white1@gmail.com>
2022-10-17 11:20:48 +0000

    Add plant details page, views and urls to plants app

--- plants/views.py
+++ plants/views.py
@@ -10,7 +10,7 @@ from .models import Plant, PlantDifficulty, PlantType
 
 
 def all_plants(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all plants, including sorting and search queries """
 
     plants = Plant.objects.all()
 
@@ -19,3 +19,15 @@ def all_plants(request):
     }
 
     return render(request, 'plants/plants.html', context)
+
+
+def plant_detail(request, plant_id):
+    """View to render an indvidual plant page"""
+
+    plant = get_object_or_404(Plant, pk=plant_id)
+
+    context = {
+        "plant": plant
+    }
+
+    return render(request, "plants/plant_detail.html", context)
