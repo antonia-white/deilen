c65d471ec08295285d74c938c8db111e7e85062a
Toni <antonia.c.white1@gmail.com>
2022-10-18 15:56:39 +0000

    Create context for the bag app to store bag information

---
+++ bag/contexts.py
@@ -0,0 +1,46 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from plants.models import Plant
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+
+            plant = get_object_or_404(Plant, pk=item_id)
+            total += item_data * plant.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'plant': plant,
+            }
+        )
+        
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
