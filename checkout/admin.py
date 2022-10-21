b92640d6c05ee505555a5b8e8170fd9328b8ebe4
Toni <antonia.c.white1@gmail.com>
2022-10-21 13:47:50 +0000

    Customise checkout admin page in admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import FullOrder, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ("lineitem_total",)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        "order_number",
+        "date",
+        "order_total",
+        "delivery_cost",
+        "grand_total"
+    )
+
+    fields = (
+        "order_number",
+        "full_name",
+        "email",
+        "phone_number",
+        "country",
+        "postcode",
+        "town_or_city",
+        "street_address1",
+        "street_address2",
+        "county",
+        "date",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+    )
+
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(FullOrder, OrderAdmin)
