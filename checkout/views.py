f654f41424a5ce50620d9aec8f7593dfab9dbd67
Toni <antonia.c.white1@gmail.com>
2022-10-25 14:07:21 +0000

    Add template and order form to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 
 def checkout(request):
     """
     A view to return the checkout page.
     """
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There are currently no items in your bag.")
+        return redirect(reverse('plants'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
