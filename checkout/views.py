142bc2a601a028de3d72316e6a728e818e536eea
Toni <antonia.c.white1@gmail.com>
2022-11-01 11:34:43 +0000

    Start implementing stripe payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
+import stripe
 
 def checkout(request):
     """
     A view to return the checkout page.
     """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There are currently no items in your bag.")
