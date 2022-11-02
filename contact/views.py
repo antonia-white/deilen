16b07e76d628ab5359a1c0f088018ddaa70c6f2f
Toni <antonia.c.white1@gmail.com>
2022-11-02 16:55:19 +0000

    Update view_contact view to render form context

--- contact/views.py
+++ contact/views.py
@@ -5,4 +5,10 @@ def view_contact(request):
     """
     A view to return the contact page.
     """
-    return render(request, 'contact/contact.html')
+    contact_form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, template, context)
