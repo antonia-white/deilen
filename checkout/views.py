a8b550dd5e5046a30ed476b43b7554210ef05533
Toni <antonia.c.white1@gmail.com>
2022-11-05 13:31:36 +0000

    Move stripe keys into checkout view context

--- checkout/views.py
+++ checkout/views.py
@@ -12,9 +12,6 @@ def checkout(request):
     A view to return the checkout page.
     """
 
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
-
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There are currently no items in your bag.")
@@ -24,6 +21,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'stripe_secret_key': settings.STRIPE_SECRET_KEY,
     }
 
     return render(request, template, context)
