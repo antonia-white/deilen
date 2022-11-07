42729fda44a81a6c49df01ee1fa22a52703ca7eb
Toni <antonia.c.white1@gmail.com>
2022-11-07 17:09:54 +0000

    Create user profile model in models.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """
+    Hold user profile information including default
+    delivery information and past order history.
+
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True
+    )
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True
+    )
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True
+    )
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True
+    )
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(
+        blank_label="Country", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+# On user object save automatically create or update
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Method to create or update user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # For existing users - only save
+    instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def profile(request):
     """
     Display the user's profile page.
