dc77e8b2a8e86146af85387ca8e03d346ee7f88f
Toni <antonia.c.white1@gmail.com>
2022-11-08 18:02:14 +0000

    Prevent toasts bag preview from showing when in profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -9,14 +10,21 @@ def profile(request):
     Display the user's profile page.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
-    orders=profile.orders.all()
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully.')
 
     form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form':form,
-        'orders':orders,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
