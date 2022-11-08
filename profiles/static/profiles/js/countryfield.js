c09da69e23bf34785603e1dc7c8eb3696895f18d
Toni <antonia.c.white1@gmail.com>
2022-11-08 14:06:40 +0000

    Style profile default delivery info form

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#64677A');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#64677A');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
