0221b7ecaeded3017d75b0195c8ea752ab9b3fd2
Toni <antonia.c.white1@gmail.com>
2022-11-08 19:36:23 +0000

    Restyle input edges accross site

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,5 +30,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs["placeholder"] = placeholder
-            self.fields[field].widget.attrs["class"] = "border-dark rounded-0 profile-form-input mt-2"
+            self.fields[field].widget.attrs["class"] = "border-dark rounded-1 profile-form-input mt-2"
             self.fields[field].label = False
