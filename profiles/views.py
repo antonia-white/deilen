b5f658476dcb3f45c0dcee069cb15b487f956442
Toni <antonia.c.white1@gmail.com>
2022-11-08 13:20:21 +0000

    Update profile view to return form and orders context

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -8,10 +9,14 @@ def profile(request):
     Display the user's profile page.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    orders=profile.orders.all()
+
+    form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form':form,
+        'orders':order,
     }
 
     return render(request, template, context)
