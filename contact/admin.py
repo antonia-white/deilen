64aaeff15a7298fa448d071500f6991d1ca89c6a
Toni <antonia.c.white1@gmail.com>
2022-11-09 13:33:37 +0000

    Add customer messages to admin in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+
+    readonly_fields = (
+        "name",
+        "from_email",
+        "phone_number",
+        "message",
+    )
+
+    fields = (
+        "name",
+        "from_email",
+        "phone_number",
+        "message",
+    )
+
+    list_display = (
+        "name",
+        "from_email",
+        "phone_number",
+        "message",
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
