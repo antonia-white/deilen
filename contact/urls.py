038571e9444a1587b5d7fc2200640920f2efef4e
Toni <antonia.c.white1@gmail.com>
2022-11-09 13:35:02 +0000

    Add contact_success url to urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.view_contact, name='contact'),
+    path(
+        "contact_success",
+        views.contact_success,
+        name="contact_success",
+    ),
 ]
