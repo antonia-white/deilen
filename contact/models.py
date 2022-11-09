5bf739b8f694a4923f6b7c518ad4bae3056515f5
Toni <antonia.c.white1@gmail.com>
2022-11-09 13:34:39 +0000

    Update contact verbose name to display for admin in models.py

--- contact/models.py
+++ contact/models.py
@@ -5,6 +5,10 @@ class Contact(models.Model):
     """
     Holds contact information and communication
     """
+
+    class Meta:
+        verbose_name_plural = 'Customer messages'
+
     name = models.CharField(max_length=50, null=False, blank=False)
     from_email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
