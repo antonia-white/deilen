c44ccaf156e866a8d6962bc73c52ad929da1482e
Toni <antonia.c.white1@gmail.com>
2022-11-10 12:12:18 +0000

    Fix typo in countryfields.js for validation

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -3,7 +3,7 @@
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
     $('#id_default_country').css('color', '#64677A');
-};
+}
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
