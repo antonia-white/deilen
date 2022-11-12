ff9d1f5d838f8b9b871f8e57030fed44ce5ec8e8
Toni <antonia.c.white1@gmail.com>
2022-11-12 09:41:50 +0000

    Refactor free delivery threshold

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
     """
     A view to return the index page.
     """
-    return render(request, 'home/index.html')
+    context = {
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+    }
+
+    template = 'home/index.html'
+
+    return render(request, template, context)
